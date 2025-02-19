f6a2d963378866cdaa6c245c582407322ac3e4f9
Mike Cousins <mike@mikecousins.com>
2025-02-19 14:54:29 -0700

    Updating to tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
