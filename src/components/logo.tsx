2e68bda309ac83569b777a97c4a2fe1027aaa105
Mike Cousins <mikecousins@gmail.com>
2025-03-27 18:18:42 -0600

    Cleaning up logo file

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,10 @@
 'use client'
 
 import { RocketLaunchIcon } from '@heroicons/react/24/outline'
-import { clsx } from 'clsx'
-import { motion } from 'framer-motion'
 
 export function Logo({ className }: { className?: string }) {
-
   return (
-    <div className="flex gap-2 items-center">
+    <div className="flex items-center gap-2">
       <RocketLaunchIcon className="h-8 w-8 text-cyan-500" />
       <span className="text-3xl">Foos</span>
     </div>
